a9840fc38d230bd7a1a8a859f50ee4fb85d9fe91
Vladyslav <dr.tienam123@gmail.com>
2024-04-16 11:54:07 +0300

    35423

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles/index.css';
+import App from '@/App.jsx';
+import {GlobalStyle} from '@/GlobalStyle.js';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <>
-    <h1>Hello</h1>
-    </>
-  </React.StrictMode>,
-)
+
+ReactDOM.createRoot(document.getElementById('wrapper'))
+    .render(
+        <React.StrictMode>
+          <App/>
+          <GlobalStyle/>
+        </React.StrictMode>,
+    );
